bcca3782297e4f89d8141e1e6d6d1965fdb6b36a
elbica <sh0705k@gmail.com>
2021-08-28 22:43:43 +0900

    feat : add jwt and editProfile, login mutations

--- schema/user/user.mutations.ts
+++ schema/user/user.mutations.ts
@@ -1,5 +1,6 @@
 import client from '../client';
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 export default {
   Mutation: {
@@ -24,6 +25,27 @@ export default {
           password: uglyPassword,
         },
       });
+    },
+    login: async (_, {username, password}) => {
+      const user = await client.user.findFirst({where: {username}});
+      if (!user) {
+        return {
+          ok: false,
+          error: 'User not found.',
+        };
+      }
+      const passwordOk = await bcrypt.compare(password, user.password);
+      if (!passwordOk) {
+        return {
+          ok: false,
+          error: 'Incorrect password',
+        };
+      }
+      const token = jwt.sign({id: user.id}, process.env.SECRET_KEY);
+      return {
+        ok: true,
+        token,
+      };
     },
   },
 };
